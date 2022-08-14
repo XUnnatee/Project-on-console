0511fc70ef5e22e651ff2e7a7b71c6e2a4ab4450
XUnnatee <98157500+XUnnatee@users.noreply.github.com>
2022-08-14 23:13:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var title = "My first console project";
+console.log(title);
+
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+console.log(first_name , last_name);
+ var a = 5;
+ var b = 10;
+ var add = a + b;
+ console.log(add)
+ var sub = b - a;
+ console.log(sub)
+ var multiply = a * b;
+ console.log(multiply)
+ var div = b / a;
+ console.log(div)
